832b39bb0cb9e7148eec95b49bb5f4ee1f54093f
彭博（pengbo02）-技术产品中心 <pengbo02@qiyi.com>
2019-11-29 18:02:42 +0800

    reademe

---
+++ README.md
@@ -0,0 +1,44 @@
+# Face relighting in Python
+A python implementation of [portrait lighting transfer using a mass transport approach](https://www3.cs.stonybrook.edu/~cvl/content/papers/2017/shu_tog2017.pdf).\
+3DMM model in original implementation is replaced by [PRNet](https://github.com/YadiraF/PRNet).
+
+# Examples
+input img | reference img | relighting img
+![img](imgs/portrait_o1.jpg)
+![img](imgs/portrait_o2.jpg)
+
+# Run
+```bash
+/root/python-portrait-relight/imgs/portrait_s1.jpg: 800x800x3
+/root/python-portrait-relight/imgs/portrait_r1.jpg: 873x799x3
+relight time: 33.05
+
+/root/python-portrait-relight/imgs/portrait_s1.jpg: 800x800x3
+/root/python-portrait-relight/imgs/portrait_r2.jpg: 950x950x3
+relight time: 35.80
+```
+Most time is spent on PRNet, which rely on dlib to detect human faces. 
+Replacing the face detector will dramatically improve the speed.
+
+# Methods
+
+Let input image be I, reference image be R and output image be O.\
+Let posI, posR be frontal 3d face position map of img I, R, with shape=[n, 3].\ 
+Let colorI, colorR be rgb colors of the reconstructed vertices of img I, R, with shape=[n, 3].\
+Let normalI, normalR be normal vectors of the vertices of img I, R, with shape=[n, 3].\
+We obtain features fI=[colorI, posI[:,:,:2], nomralI], fR=[colorR, posR[:,:,:2], normalR] of img I, R, with shape=[n, 8].\
+Then we determine pdf transfer function t, so that f{t(fI)}=f{fR}, where f{x} is the probability density function of array x.\
+t(colorI) is the relighted image of I with R for reference.\
+Finally, we apply regrain algorithm for postprocessing.
+
+# Dependency
+Process of pdf transfer and regrain are introduced in [Automated colour grading using colour distribution transfer](http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.458.7694&rep=rep1&type=pdf) by F. Pitie , A. Kokaram and R. Dahyot.
+This repository denpends on my [python implementation of this paper](https://github.com/pengbo-learn/python-color-transfer).
+
+
+# References
+[portrait lighting transfer using a mass transport approach](https://www3.cs.stonybrook.edu/~cvl/content/papers/2017/shu_tog2017.pdf) 
+by Zhixin Shu, Sunil Hadap, Eli Shechtman, Kalyan Sunkavalli, Sylvain Paris and Dimitris Samaras.\
+[Author's matlab implementation](https://github.com/AjayNandoriya/PortraitLightingTransferMTP)
+
+
--- relight.py
+++ relight.py
@@ -9,12 +9,15 @@ Author's Matlab Implementation:
 import os
 import sys
 sys.path.insert(0, './PRNet')
+sys.path.insert(0, './python_color_transfer')
 
 import cv2
+import time
 import numpy as np
 
 from PRNet.api import PRN
-from python_color_transfer.color_transfer import ColorTransfer
+from face3d.face3d.mesh.render import render_colors as render_texture
+from python_color_transfer.color_transfer import ColorTransfer, Regrain
 
 def _normalize_v3(arr):
     ''' Normalize a numpy array of 3 component vectors shape=(n,3) '''
@@ -24,17 +27,24 @@ def _normalize_v3(arr):
     arr[:,2] /= lens                
     return arr
 
+def frontalize(vertices):
+    '''copy from PRNet, edit npy path. rotate 3d face points to frontal pose.'''
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    npy_path = os.path.join(cur_dir, 'PRNet/Data/uv-data/canonical_vertices.npy')
+    canonical_vertices = np.load(npy_path)
+    vertices_homo = np.hstack((vertices, np.ones([vertices.shape[0],1]))) #n x 4
+    P = np.linalg.lstsq(vertices_homo, canonical_vertices, rcond=None)[0].T # Affine matrix. 3 x 4
+    front_vertices = vertices_homo.dot(P.T)
+    return front_vertices
 
 class Relight:
     """Methods for relighting human faces."""
-    def __init__(self):
+    def __init__(self, m=20, c=8, smoothness=0.125):
         self.prn = PRN(is_dlib=True, prefix='PRNet') 
-        self.ct = ColorTransfer()
+        self.ct = ColorTransfer(m=m, c=c)
+        self.rg = Regrain(smoothness=smoothness)
         self.mask1d = self.prn.face_ind
-    def get_faces(self):
-        '''get faces of 3d meshes, each face is a triplet of vertice inds.'''
-        faces = self.prn.triangles
-        return faces
+        self.triangles = self.prn.triangles
     def get_normals(self, vertices=None):
         '''get normals of vertices. 
         
@@ -47,8 +57,7 @@ class Relight:
         # Create a zeroed array with the same type and shape as our vertices i.e., per vertex normal
         normals = np.zeros(vertices.shape, dtype=vertices.dtype )
         # Create an indexed view into the vertex array using the array of three indices for triangles
-        faces = self.prn.triangles
-        tris = vertices[faces]
+        tris = vertices[self.triangles]
         # Calculate the normal for all the triangles, by taking the cross product of the vectors v1-v0, 
         # and v2-v0 in each triangle             
         n = np.cross(tris[::,1]-tris[::,0], tris[::,2]-tris[::,0])
@@ -59,52 +68,129 @@ class Relight:
         # But instead of one per triangle (i.e., flat shading), we add to each vertex in that triangle, 
         # the triangles' normal. Multiple triangles would then contribute to every vertex, so we need to normalize again afterwards.
         # The cool part, we can actually add the normals through an indexed view of our (zeroed) per vertex normal array
-        normals[faces[:,0]] += n
-        normals[faces[:,1]] += n
-        normals[faces[:,2]] += n
+        normals[self.triangles[:,0]] += n
+        normals[self.triangles[:,1]] += n
+        normals[self.triangles[:,2]] += n
         _normalize_v3(normals)
         return normals
     def get_pos(self, img_arr=None):
+        '''get 3d position map by PRNet.
+        
+        Args:
+            img_arr: face img to extract 3d face position map.
+        '''
         pos = self.prn.process(img_arr)
         return pos
-
+    def regrain(self, img_arr_in=None, img_arr_col=None):
+        '''Regrain algorithm implemented in python_color_transfer.'''
+        img_arr_out = self.rg.regrain(img_arr_in=img_arr_in,
+                                      img_arr_col=img_arr_col)
+        return img_arr_out
+    def relight_features(self, features=None, ref_features=None):
+        '''Relight by applying pdf transfer.'''
+        features = features.transpose()
+        ref_features = ref_features.transpose()
+        out_features = self.ct.pdf_transfer_nd(arr_in=features, 
+                                               arr_ref=ref_features,
+                                               step_size=0.2)
+        out_features = out_features.transpose()
+        return out_features
+    def relight(self, img_arr=None, ref_arr=None):
+        '''Relight img_arr according to ref_arr.
+        
+        Args:
+            img_arr: input bgr numpy array.
+            ref_arr: reference bgr numpy array, target lighting condition.
+        Returns:
+            out_arr: relighted bgr array of img_arr.
+        '''
+        # get 3d positions by PRNet
+        pos = self.get_pos(img_arr=img_arr)
+        ref_pos = self.get_pos(img_arr=ref_arr)
+        # obtain texture by remapping
+        texture = cv2.remap(img_arr, pos[:,:,:2].astype(np.float32), 
+                            None, interpolation=cv2.INTER_NEAREST, 
+                            borderMode=cv2.BORDER_CONSTANT, borderValue=(0))
+        ref_texture = cv2.remap(ref_arr, ref_pos[:,:,:2].astype(np.float32), 
+                                None, interpolation=cv2.INTER_NEAREST, 
+                                borderMode=cv2.BORDER_CONSTANT, borderValue=(0))
+        # colors
+        colors = texture.reshape(-1, 3).astype(np.float64)[self.mask1d, :]
+        ref_colors = ref_texture.reshape(-1, 3).astype(np.float64)[self.mask1d, :]
+        # vertices
+        vertices = pos.reshape(-1, 3)[self.mask1d, :]
+        ref_vertices = ref_pos.reshape(-1, 3)[self.mask1d, :]
+        # frontalize
+        vertices_f = frontalize(vertices)
+        ref_vertices_f = frontalize(ref_vertices)
+        # normals
+        normals = self.get_normals(vertices=vertices_f)
+        ref_normals = self.get_normals(vertices=ref_vertices_f)
+        # normalize 
+        colors_n = colors / 255.
+        ref_colors_n = ref_colors / 255.
+        vertices_n = vertices_f - vertices_f.min()
+        vertices_n = vertices_n / (vertices_n.max() + 1e-6)
+        ref_vertices_n = ref_vertices_f - ref_vertices_f.min()
+        ref_vertices_n = ref_vertices_n / (ref_vertices_n.max() + 1e-6)
+        # concatenate
+        features = np.concatenate((colors_n, vertices_n[:,:2], normals), axis=1)
+        ref_features = np.concatenate((ref_colors_n, ref_vertices_n[:,:2], ref_normals), axis=1)
+        # relighting by color transfer
+        out_features = self.relight_features(features=features, ref_features=ref_features)
+        out_colors = out_features[:, :3]
+        out_colors[out_colors < 0] = 0
+        out_colors[out_colors > 1] = 1
+        out_colors = (out_colors*255).astype('uint8')
+        # render
+        [height, width, _] = img_arr.shape
+        out_arr = render_texture(vertices=vertices.T, colors=out_colors.T, 
+                                 triangles=self.triangles.T, h=height, w=width, c=3)
+        # mask
+        visible_colors = np.ones((vertices.shape[0], 1))
+        face_mask = render_texture(vertices=vertices.T, colors=visible_colors.T, 
+                                   triangles=self.triangles.T, h=height, w=width, c=1)
+        face_mask = np.squeeze(face_mask > 0).astype(np.float32)
+        # replace
+        out_arr = img_arr*(1 - face_mask[:,:,np.newaxis]) + out_arr*face_mask[:,:,np.newaxis]
+        # regrain
+        out_arr = self.regrain(img_arr_in=img_arr, img_arr_col=out_arr)
+        return out_arr
+        
 
 def demo():
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     # path
     cur_dir = os.path.dirname(os.path.abspath(__file__))
     img_folder = os.path.join(cur_dir, 'imgs')
-    img_path = os.path.join(img_folder, 'portrait_s1.jpg')
-    ref_path = os.path.join(img_folder, 'portrait_r1.jpg')
+    img_names = ['portrait_s1.jpg', 'portrait_s1.jpg']
+    ref_names = ['portrait_r1.jpg', 'portrait_r2.jpg']
+    out_names = ['portrait_o1.jpg', 'portrait_o2.jpg']
+    img_paths = [os.path.join(img_folder, x) for x in img_names]
+    ref_paths = [os.path.join(img_folder, x) for x in ref_names]
+    out_paths = [os.path.join(img_folder, x) for x in out_names]
     # cls init
-    RT = Relight()
-    # inputs
-    img_arr = cv2.imread(img_path)
-    ref_arr = cv2.imread(ref_path)
-    # get 3d positions by PRNet
-    pos = RT.get_pos(img_arr=img_arr)
-    ref_pos = RT.get_pos(img_arr=ref_arr)
-    # obtain texture by remapping
-    texture = cv2.remap(img_arr, pos[:,:,:2].astype(np.float32), 
-                        None, interpolation=cv2.INTER_NEAREST, 
-                        borderMode=cv2.BORDER_CONSTANT, borderValue=(0))
-    ref_texture = cv2.remap(ref_arr, pos[:,:,:2].astype(np.float32), 
-                            None, interpolation=cv2.INTER_NEAREST, 
-                            borderMode=cv2.BORDER_CONSTANT, borderValue=(0))
-    # colors, vertices, normals for color transfer
-    colors = texture.reshape(-1, 3).astype(np.float64)
-    ref_colors = ref_texture.reshape(-1, 3).astype(np.float64)
-    vertices = pos[:,:,:2].reshape(-1, 2)
-    ref_vertices = ref_pos[:,:,:2].reshape(-1, 2)
-    normals = RT.get_normals(vertices=vertices)
-    ref_normals = RT.get_normals(vertices=ref_vertices)
-    features = np.concatenate((colors, vertices, normals), axis=1)
-    ref_features = np.concatenate((ref_colors, ref_vertices, ref_normals), axis=1)
-    assert features.shape == ref_features.shape
-    assert features.dtype == ref_features.dtype
-    # relighting by color transfer
-    import pdb
-    pdb.set_trace()
+    RT = Relight(c=8)
+    for img_path, ref_path, out_path in zip(img_paths, ref_paths, out_paths):
+        # inputs
+        img_arr = cv2.imread(img_path)
+        [h, w, c] = img_arr.shape
+        print('{}: {}x{}x{}'.format(img_path, h, w, c))
+        ref_arr = cv2.imread(ref_path)
+        [h, w, c] = ref_arr.shape
+        print('{}: {}x{}x{}'.format(ref_path, h, w, c))
+        # relight
+        stime = time.time()
+        out_arr = RT.relight(img_arr=img_arr, ref_arr=ref_arr)
+        print('relight time: {:.2f}\n'.format(time.time()-stime))
+        # resize for display
+        [ref_height, ref_width, _] = ref_arr.shape
+        [height, width, _] = img_arr.shape
+        ref_width = int(ref_width*height/ref_height)
+        ref_height = height
+        ref_arr = cv2.resize(ref_arr, (ref_width, ref_height))
+        # save
+        cv2.imwrite(out_path, np.concatenate((img_arr, ref_arr, out_arr), axis=1))
 
 if __name__ == '__main__':
     demo()
